34f704c26bc5ddc1a51b6edf240108b621867dec
your-github-username <your-email@example.com>
2025-06-01 16:21:39 +0530

    configured & setup frontend dependencies , configured routes , created the landing page hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import { Route, Routes, useNavigate } from "react-router";
+import { useEffect } from "react";
+import HeroSection from "./pages/LandingPage/LandingPage";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
-export default function App() {
+// Components
+
+function App() {
+  const navigate = useNavigate();
+  // useEffect(() => {
+  //   localStorage.getItem("user") === null
+  //     ? navigate("/landingPage")
+  //     : navigate("/dashboard");
+  // }, [navigate]);
+
   return (
-    <div className="App min-h-screen bg-blue-100 flex flex-col items-center justify-center text-center p-6">
-      <h1 className="text-4xl font-bold text-blue-700 mb-4">
-        Welcome to the React App
-      </h1>
-      <p className="text-lg text-gray-700 mb-2">
-        This is a simple React application.
-      </p>
-      <div className="bg-green-200 text-green-800 px-4 py-2 rounded-lg shadow mt-4">
-        🎉 Congrats on setting this up!
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<h1 className="bg-red-500 ">Home Page</h1>} />
+        <Route path="/landingPage" element={<HeroSection />} />
+        <Route path="/dashboard" element={<h1>About Page</h1>} />
+        <Route path="/project/:id" element={<h1>Project Details Page</h1>} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </>
   );
 }
+
+export default App;
